1122db24520cf5b983b9fffca01126d57e33f419
Kris <Kris@Kris-PC>
2013-05-19 20:17:01 +1200

    Initial commit

---
+++ main.c
@@ -0,0 +1,227 @@
+/* 
+ * File:   main.c
+ * Author: Kris
+ *
+ * Created on February 10, 2013, 9:08 PM
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+#include "config.h"
+#include "routines.h"
+
+// generic macro & constants
+#define BAUD_RATE               19200
+#define _XTAL_FREQ              8000000ul // i.e. 8 MHz Fosc
+#define TMR1_MAX_COUNT          0xfffful
+#define TMR1_PRESCALAR          8 // i.e. assume 1:8 TMR1 prescalar
+#define TMR2_PRESCALAR          4 // i.e. assume 1:4 TMR2 prescalar
+#define COUNTS_PER_T_100us(T,S) ((T * _XTAL_FREQ) / (4 * S * 10000ul)) // i.e. TMR clock = (Fosc/4)
+
+// servo specific constants
+#define _20ms_PWM_PERIOD        COUNTS_PER_T_100us(200, TMR1_PRESCALAR) // i.e. 200 * 100us = 20ms
+#define MAX_PULSE_WIDTH         COUNTS_PER_T_100us(27, TMR1_PRESCALAR) // i.e. 27 * 100us = 2.7ms
+#define MIN_PULSE_WIDTH         COUNTS_PER_T_100us(5, TMR1_PRESCALAR) // i.e. 5 * 100us = 0.5ms
+#define MAX_SERVO_POS           0x3f
+#define SERVO_POS_2_WIDTH       ((MAX_PULSE_WIDTH - MIN_PULSE_WIDTH) / MAX_SERVO_POS)
+
+// motor ctrl specific constants
+#define _100us_PWM_PERIOD       COUNTS_PER_T_100us(1, TMR2_PRESCALAR) // i.e. 100us or 10KHz
+#define _100us_PERIOD_10BIT_RES COUNTS_PER_T_100us(1, TMR2_PRESCALAR/4) // i.e. assuming a 10-bit counter is being used
+#define MAX_MOTOR_SPEED         0x1f
+
+#define MAX_BUFFER_SIZE         30
+char putch_buffer[MAX_BUFFER_SIZE];
+int head = 0;
+int tail = 0;
+
+char servo_pos = 0;
+int pwm_pulse_width = 0;
+int pwm_state_tmr_presets[2]; // array of pwm OFF[0]/ON[1] timer presets
+char pwm_current_state = 0; // current output state i.e. ON[1] or OFF[0]
+int pwm_cycle_cnt_debug = 0;
+
+int motor_speed = 0;
+int motor_duty_cycle = 0;
+
+void init_osc() {
+    // setup INTRC frequency
+    OSCCONbits.IRCF = 0b111; // enable 8 Mhz internal clock
+    while (!IOFS)
+        continue; // wait for frequency stable flag
+}
+
+void init_uart() {
+    SPBRG = 25; // (_XTAL_FREQ / BAUD_RATE / 16) - 1;
+    BRGH = 1; // high speed
+    SYNC = 0; // asynchronous mode
+    TX9 = 0;  // 8-bit frames
+    SPEN = 1; // enable  serial port
+    TXEN = 1; // enable transmitter
+    CREN = 1; // enable receiver
+
+    RCIF = 0; // clear any RX interrupts
+    RCIE = 1; // enable RX interrupts
+}
+
+void init_interrupts() {
+    GIE = 1; // global interrupt enabled
+    PEIE = 1; // peripheral interrupts enabled
+}
+
+/*------------------------ UART IO routines -----------------------*/
+
+void putch(char data) {
+    putch_buffer[tail++] = data;
+    if (tail == MAX_BUFFER_SIZE) {
+        tail = 0;
+    }
+    if (!TXIE) {
+        TXIE = 1;
+    }
+}
+
+/*------------------------ Servo PWM Speed Controller -----------------------*/
+
+#pragma interrupt_level 1
+void set_servo_position(char pos) {
+    servo_pos = pos & MAX_SERVO_POS;
+    pwm_pulse_width = MIN_PULSE_WIDTH + SERVO_POS_2_WIDTH * servo_pos;
+    // set the ON (pulse) time preset
+    pwm_state_tmr_presets[1] = TMR1_MAX_COUNT - pwm_pulse_width;
+    // set OFF time preset
+    pwm_state_tmr_presets[0] = TMR1_MAX_COUNT - _20ms_PWM_PERIOD - pwm_pulse_width;
+}
+
+#pragma interrupt_level 1
+void apply_next_pwm_state() {
+    char next_state = ++pwm_current_state % 2; // set output to next state
+    RA1 = next_state; // output next state
+    TMR1 = pwm_state_tmr_presets[next_state]; // apply next state timer preset
+    pwm_current_state = next_state; // save next state as current
+}
+
+void init_servo_pwm_output() {
+    TRISA1 = 0; // set RA1 as servo PWM position output
+    set_servo_position((MAX_SERVO_POS + 1) / 2); // init pulse width settings to 50% before next call
+    apply_next_pwm_state();
+}
+
+/**
+ * TMR1 used for servo PWM waveform timing
+ */
+void init_timer1() {
+    TMR1CS = 0; // internal clock source (FOSC/4)
+    T1CONbits.T1CKPS = 0b11; // apply 1:8 prescalar (gives 250 KHz from 8Mhz FOSC)
+    TMR1ON = 1; // enable TMR1
+    TMR1IF = 0; // clear TMR1 interrupt flag
+    TMR1IE = 1; // enable interrupts for TMR1 overflow
+}
+
+/*------------------------ Motor PWM Speed Controller -----------------------*/
+
+#pragma interrupt_level 1
+void set_motor_speed_and_dir(char speed_and_dir) {
+    RB1 = (speed_and_dir >> 5) & 0x1; // set motor direction using bit 5
+    motor_speed = speed_and_dir & MAX_MOTOR_SPEED; // set motor speed using bits 4:0
+    motor_duty_cycle = (_100us_PERIOD_10BIT_RES * motor_speed) / MAX_MOTOR_SPEED;
+    CCPR1L = motor_duty_cycle >> 2; // set duty cycle reg top 8 most significant bits
+    CCP1CON = ((0b11 & motor_duty_cycle) << 4) | (0xf & CCP1CON); // set duty cycle reg bottom 2 least significant bits
+}
+
+void init_ccp() {
+    // enable compare mode
+    CCP1CONbits.CCP1M = 0b1100; // CCP module in PWM mode
+}
+
+void init_motor_ctrl_outputs() {
+    TRISB0 = 0; // enable PWM output on RB0/CCP1
+    TRISB1 = 0; // set RB1 as motor direction output
+    set_motor_speed_and_dir(0);
+}
+
+void init_timer2() {
+    T2CONbits.T2CKPS = 0b01; // 1:4 prescalar
+    PR2 = _100us_PWM_PERIOD;
+    TMR2IE = 0; // disable TMR2 interrupts
+    TMR2ON = 1; // enable TMR2
+}
+
+/*------------------------ sys management routines -----------------------*/
+
+void interrupt handle_int() {
+    // transition servo PWM output state
+    if (TMR1IE && TMR1IF) {
+        TMR1IF = 0;
+        apply_next_pwm_state();
+        pwm_cycle_cnt_debug++;
+        return;
+    }
+
+    // received servo position on serial
+    if (RCIE && RCIF) {
+        // read data & clear interrupt flag
+        char ctrl_byte = RCREG;
+        if (ctrl_byte & 0x40) {
+            set_motor_speed_and_dir(ctrl_byte);
+        } else {
+            set_servo_position(ctrl_byte);
+        }
+        return;
+    }
+
+    // write uart data on request
+    if (TXIE && TXIF) {
+        if (head != tail) {
+            TXREG = putch_buffer[head++];
+            if (head == MAX_BUFFER_SIZE) {
+                head = 0;
+            }
+        } else {
+            TXIE = 0;
+        }
+        return;
+    }
+}
+
+void bootstrap() {
+    //init_osc();
+    init_interrupts();
+    init_uart();
+
+    // init motor pwm
+    init_ccp();
+    init_timer2();
+    init_motor_ctrl_outputs();
+
+    // init servo pwm
+    init_servo_pwm_output();
+    init_timer1();
+}
+
+
+int main() {
+    bootstrap();
+
+    printf("SERVO >>>>>\r\n");
+    printf("_20ms_PWM_PERIOD: %lu\r\n", _20ms_PWM_PERIOD);
+    printf("MAX_PULSE_WIDTH: %u\r\n", MAX_PULSE_WIDTH);
+    printf("MIN_PULSE_WIDTH: %u\r\n", MIN_PULSE_WIDTH);
+    printf("MOTOR >>>>\r\n");
+    printf("_100us_PWM_PERIOD: %u\r\n", _100us_PWM_PERIOD);
+
+    while (1) {
+        printf("SERVO >> pos: %d, pulse-width: %u, cycle-count: %u\r\n",
+                servo_pos, pwm_pulse_width, pwm_cycle_cnt_debug);
+//        printf("SERVO >> on-time-preset: %u, off-time-preset: %u\r\n",
+//                pwm_state_tmr_presets[1],
+//                pwm_state_tmr_presets[0]);
+        _delay(4000000);
+        printf("MOTOR >> speed: %d, direction: %d, duty-cycle: %u\r\n",
+                motor_speed, RB1, motor_duty_cycle);
+        _delay(4000000);
+    }
+    
+    return (EXIT_SUCCESS);
+}
+
---
+++ routines.h
@@ -0,0 +1,23 @@
+/* 
+ * File:   routines.h
+ * Author: Kris
+ *
+ * Created on February 26, 2013, 10:17 PM
+ */
+
+#ifndef ROUTINES_H
+#define	ROUTINES_H
+
+#ifdef	__cplusplus
+extern "C" {
+#endif
+
+extern unsigned char bit_count(unsigned char a);
+
+
+#ifdef	__cplusplus
+}
+#endif
+
+#endif	/* ROUTINES_H */
+
